f627c42d00552d9b3f480189c48866d9fd59ccf7
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-10-17 15:16:44 +0300

    add simple wrapper function to plot two columns

--- R/daplot.R
+++ R/daplot.R
@@ -6,6 +6,25 @@
 #' @export
 #'
 #' @examples
-daplot <- function(dat){
-  return(dat)
+daplot <- function(dat, x_val, y1_val, y2_val, y1_label = NULL, y2_label = NULL) {
+  stopifnot(is.data.frame(dat))
+
+  # Capture column names as strings for checking
+  x_col <- rlang::as_name(rlang::enquo(x_val))
+  y1_col <- rlang::as_name(rlang::enquo(y1_val))
+  y2_col <- rlang::as_name(rlang::enquo(y2_val))
+
+  stopifnot(x_col %in% names(dat))
+  stopifnot(y1_col %in% names(dat))
+  stopifnot(y2_col %in% names(dat))
+
+
+  plot_dat <- dat
+
+  pl <- plot_dat |>
+    ggplot2::ggplot(ggplot2::aes(x = {{ x_val }})) +
+    ggplot2::geom_line(ggplot2::aes(y = {{ y1_val }}, color = "y1")) +
+    ggplot2::geom_line(ggplot2::aes(y = {{ y2_val }}, color = "y2"))
+
+  return(pl)
 }
